7c473054ae719d6cd61f52e765be4414ba2d44e7
Juan <juanbedon26@hotmail.com>
2018-06-08 12:06:37 -0500

    Third version

--- js/app.js
+++ js/app.js
@@ -1,26 +1,25 @@
 var array = [1,2,3,4,5,6,7,8,9,0];
 
-    var i,j,k;
-    for (i = array.length; i; i--) {
-        j = Math.floor(Math.random() * i);
-        k = array[i - 1];
-        array[i - 1] = array[j];
-        array[j] = k;
-    }
+var i,j,k;
+for (i = array.length; i; i--) {
+    j = Math.floor(Math.random() * i);
+    k = array[i - 1];
+    array[i - 1] = array[j];
+    array[j] = k;
+}
 
 var randomNumber = array.slice(0,4).toString().replace(",","").replace(",","").replace(",","").replace(",","");
 
 console.log(randomNumber)
 
-
 $('form').on('submit', function(e) {
 
-  if ( $('input').val().length < 4 || $('input').val().length > 4 ) {
+  if ( $('input').val() == /(?!.*(\d).*\1)\d{4}/ ) {
 
     $('.error').show();
     $('input').val('')
 
-  } else if ( $('input').val() == /(?!.*(\d).*\1)\d{4}/ ) {
+  } else if ( $('input').val().length < 4 || $('input').val().length > 4 ) {
 
     $('.error').show();
     $('input').val('')
